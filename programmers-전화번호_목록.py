f65329d3dcd759332fb5fac2d35c4e44510557d2
Yoonsang's Record <yanggak12@naver.com>
2022-02-28 01:55:58 +0900

    Update programmers-전화번호_목록.py

--- programmers-전화번호_목록.py
+++ programmers-전화번호_목록.py
@@ -1,6 +1,6 @@
 def solution(phone_book):
     p_book = sorted(phone_book)
-    for i, j in zip(p_book, p_book[1:]):
-        if str(j).startswith(i):
+    for i in range(len(p_book)-1):
+        if str(p_book[i+1]).startswith(str(p_book[i])):
             return False
     return True
